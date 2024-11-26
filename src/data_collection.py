403bb721a798f7c999b4503f1a90df81b489c9dd
Nikendra Shekhawat <146007305+nikendrashekhawat@users.noreply.github.com>
2024-11-25 20:19:16 -0500

    add KggleDataCollection

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,7 +1,56 @@
 import tomllib
+import kaggle
 
-with open("../configs/cfg.toml", "rb") as cfg_file:
-    configs = tomllib.load(cfg_file)
+class KaggleDataCollection():
+    
+    def __init__(self, username: str, key: str):
+        self.__username = username
+        self.__key = key
+        
+    @property    
+    def username(self):
+        return self.__username
+    
+    @username.setter
+    def username(self, new_username: str):
+        self.__username = new_username
+    
+    @property    
+    def key(self):
+        return self._key
+    
+    @key.setter
+    def key(self, new_key: str):
+        self.__key = new_key
+    
+    
+    def __kaggle_auth0(self):
+        api = kaggle.api
+        api.CONFIG_NAME_USER = self.__username
+        api.CONFIG_NAME_KEY = self.__key
+        return api
+    
+    def get_kaggle_dataset(self, dataset: str, filename: str, path: str = "dataset/", force: bool = False):
+        api = self.__kaggle_auth0()
+        api.dataset_download_file(
+            dataset=dataset, 
+            file_name=filename,
+            path=path,
+            force=force
+            )
+        return None
 
-KAGGLE_USERNAME = configs["kaggle"]["USERNAME"]
-KAGGLE_APIKEY = configs["kaggle"]["KEY"]
+if __name__ == "__main__":
+    
+    with open("configs/cfg.toml", "rb") as cfg:
+        configs = tomllib.load(cfg)
+    
+    KAGGLE_USERNAME = configs["kaggle"]["username"]
+    KAGGLE_APIKEY = configs["kaggle"]["key"]  
+    
+    kdc = KaggleDataCollection(KAGGLE_USERNAME, KAGGLE_APIKEY)
+    kdc.get_kaggle_dataset("lukebarousse/data-analyst-job-postings-google-search", "gsearch_jobs.csv")
+    print(kdc._username)
+    print(kdc._key)
+    kdc.get_kaggle_dataset
+    
