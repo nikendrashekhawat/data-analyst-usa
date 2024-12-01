a90cd53d8b7456c2a7604c372e71ad6cad4451c1
Nikendra Shekhawat <146007305+nikendrashekhawat@users.noreply.github.com>
2024-12-01 18:15:32 -0500

    update tools

--- utils/tools.py
+++ utils/tools.py
@@ -1,20 +1,49 @@
-from typing import List
-from functools import cache
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.tokenize.mwe import MWETokenizer
-from nltk.corpus import stopwords
+import re
+from nltk.tokenize import MWETokenizer
 
-nltk.download('punkt_tab')
+from utils.reserved_keywords import (
+    technical_skills,
+    soft_skills,
+    technical_mwe,
+    soft_skills_mwe,
+    education_requirement,
+    education_mwe
+)
+
+# Combine all multi-word expressions (MWEs) and keywords
+combined_mwes = technical_mwe + soft_skills_mwe + education_mwe
+keywords = set(combined_mwes + technical_skills + soft_skills + education_requirement)
+
+# Translation table for replacing punctuation with whitespace
+punctuations = "!$%'(),-./:;?[\\]^_`{|}"
+translate_table = str.maketrans(punctuations, ' ' * len(punctuations))
 
 
-@cache
-def get_word_tokens(description: str) -> List[str]:
+# Tokenize MWEs
+mwes = [tuple(phrase.split()) for phrase in combined_mwes]
+mwe_tokenizer = MWETokenizer(mwes=mwes, separator=' ')
+
+#Function to remove punctuation
+remove_punctuation = lambda text: text.translate(translate_table)
+
+# Function to remove extra spaces
+remove_extra_spaces = lambda text: re.sub(r'\s+', ' ', text).strip()
+   
+
+def get_word_tokens(text: str) -> list[str]:
     
-    tokens = word_tokenize(description.lower())
+    text = remove_punctuation(text)
+    text = remove_extra_spaces(text)
+    text = text.lower()
     
-    stop_words = set(stopwords.words('english'))
+    tokens = text.split()
     
-    filtered_tokens = [token for token in tokens if token.isalnum() and token not in stop_words]
+    new_tokens = mwe_tokenizer.tokenize(tokens)
+    
+    filtered_tokens = [token for token in new_tokens if token in keywords] 
 
-    return filtered_tokens
+    return list(set(filtered_tokens))
+
+
+
+
