44b2edae0d4b0daca71d2ad8f283b7198ed31cbd
nikendrashekhawat <146007305+nikendrashekhawat@users.noreply.github.com>
2024-12-06 23:13:19 -0500

    add categorize_tokens method to the DataCleaner class

--- utils/cleaner.py
+++ utils/cleaner.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from nltk.tokenize import MWETokenizer
-from utils.reserved_keywords import (
+from reserved_keywords import (
     technical_tokens,
     soft_skills_tokens,
     education_tokens,
@@ -12,7 +12,8 @@ technical_tokens_arr = np.array(technical_tokens, dtype=np.str_)
 softskills_tokens_arr = np.array(soft_skills_tokens, dtype=np.str_)
 educational_tokens_arr = np.array(education_tokens, dtype=np.str_)
 
-# Combine all keywords
+# Combine all keywords__all__ = ['tools']
+
 all_keywords = np.unique(np.concatenate([technical_tokens_arr, softskills_tokens_arr, educational_tokens_arr]))
 
 #Filtering multi word expressions
@@ -34,13 +35,15 @@ class DataCleaner():
     
     
     def filter_tokens(self, arr1: np.ndarray, arr2: np.ndarray) -> np.ndarray | pd._libs.missing.NAType:
+        if arr1 is pd.NA:
+            return arr1
         filtered_arr = arr1[np.isin(arr1, arr2)]
         if not np.any(filtered_arr):
             return pd.NA
         return np.unique(filtered_arr)    
         
               
-    def tokenize_words(self, text: str) -> np.ndarray | pd.libs.missing.NAType:
+    def tokenize_words(self, text: str) -> np.ndarray | pd._libs.missing.NAType:
         text = self.remove_punctuations(text)
         tokens = text.split()
         tokens = np.array(tokens, dtype=np.str_)
@@ -58,17 +61,27 @@ class DataCleaner():
     def tokenize_column(self, col: str, filter_keywords: np.ndarray = all_keywords):
         col_name = col+"_tokens"
         self.df[col_name] = self.df[col].apply(self.tokenize_words)
-        self.df[col_name] = self.df[col_name].apply(filter_keywords, args=(all_keywords,))
+        self.df[col_name] = self.df[col_name].apply(self.filter_tokens, args=(filter_keywords,))
         return self 
     
     
-    def split_tokens(self, col: str, filter_with: list[np.ndarray] = [technical_tokens_arr, softskills_tokens_arr, educational_tokens_arr], expand: bool = False):
+    def split_tokens(
+            self, 
+            col: str, 
+            filter_with: list[np.ndarray] = [technical_tokens_arr, softskills_tokens_arr, educational_tokens_arr],
+            expand: bool = False
+        ) -> pd.DataFrame:
+
         new_cols = len(filter_with)
+        print("NEW COLS", new_cols)
         cols_name = ["arr_"+ str(i) for i in range(new_cols)]
-        for index, fil_arr in zip(range(new_cols), filter_with):
-            cols_name[index] = self.df[col].apply(self.filter_tokens, args=(fil_arr,))
-        
-        self.df[col]
+        print("COLS NAME", cols_name)
+        frame = {arr: self.df[col].apply(self.filter_tokens, args=(fil_arr,)) for arr, fil_arr in zip(cols_name, filter_with)}
+    
+        if expand:
+            return pd.concat([self.df, pd.DataFrame(frame)], axis=1)
+            
+        return pd.DataFrame(frame)
         
         
     def remove_duplicates(self):
@@ -93,7 +106,11 @@ class DataCleaner():
         return self.df
     
 if __name__ == '__main__':
-    
+    data = pd.read_csv("dataset/gsearch_jobs.csv", index_col=0)
+    # ser = data["description_tokens"].head(50)
+    dc = DataCleaner(data)
+    ex = dc.tokenize_column("description").split_tokens("description_tokens")
+    print(ex.head())
 
         
     
