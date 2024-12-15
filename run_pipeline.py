08a49cab810c29652c6880f12a12b946ab1393bd
Nikendra Shekhawat <146007305+nikendrashekhawat@users.noreply.github.com>
2024-12-15 16:06:47 -0500

    update after structuring the code

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,13 +1,40 @@
+from pathlib import Path
 import pandas as pd
-from utils.pipeline import DataPipeline
+from src._struct.pipeline import DataPipeline
 
 pipeline = DataPipeline(
         kaggle_dataset="lukebarousse/data-analyst-job-postings-google-search",
         file_to_download="gsearch_jobs.csv"
     )
-
 pipeline.run_pipeline()
 file = pipeline.cleaned_filepath
 data = pd.read_parquet(file)
-print(data.columns)
-
+# Saving data into sub frames for working streamlit efficiently
+save_to = Path('./data-subframes')
+save_to.mkdir(parents=True, exist_ok=True)
+salary = data[['salary_pay', 'salary_min', 'salary_max', 'salary_average']]
+tokens = data[['description_tokens', 'technical_tokens', 'softskills_tokens', 'education_tokens']]
+cols_to_drop = list(salary.columns) + list(tokens.columns)
+jobs = data.drop(columns=cols_to_drop)
+salary.to_parquet(save_to/'salary.parquet', index=False)
+tokens.to_parquet(save_to/'tokens.parquet', index=False)
+jobs.to_parquet(save_to/'jobs.parquet', index=False)
+print(salary.columns)
+print(tokens.columns)
+print(jobs.columns)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
