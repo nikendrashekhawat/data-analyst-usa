90a4c7d36e590d7e6e4b4703aff3f7154cd491eb
Nikendra Shekhawat <146007305+nikendrashekhawat@users.noreply.github.com>
2025-01-01 03:07:14 -0500

    add 2 finalize charts

--- app.py
+++ app.py
@@ -3,57 +3,71 @@ import pandas as pd
 from strm.st_extends import count_tokens
 from strm.st_extends import plot_horizontal
 
-st.set_page_config(page_title="Data Analyst Jobs (USA)", layout="wide")
-
-st.title("Data Analyst Jobs - USA")
+st.set_page_config(page_title="Data Analyst Jobs (USA)", layout='wide')
 
 salary = pd.read_parquet("data-subframes/salary.parquet")
 skills = pd.read_parquet("data-subframes/tokens.parquet")
 jobs = pd.read_parquet("data-subframes/tokens.parquet")
-
 median_min = salary["salary_min"].median()
 median_max = salary["salary_max"].median()
 average_salary = salary["salary_avg"].mean().round(2)
 
-cont_met = st.container(border=True)
-with cont_met:
-    cont_met.markdown("<h2 style='text-align: center;'>Median Salary</h2>", unsafe_allow_html=True)
-    met_col1, met_col2, met_col3 = st.columns(3)
-    with met_col1.container():
-        st.metric('Minimum Salary', median_min)  
-    with met_col2.container():
-        st.metric('Maximum Salary', median_max)
-    with met_col3.container():
-        st.metric('Average Salary', average_salary)
+st.markdown("<h1 style='text-align: center;'>Data Analyst Jobs - USA</h1>", unsafe_allow_html=True)
+
+st.markdown("<br><br>", unsafe_allow_html=True)
     
 technical_skills = count_tokens(skills["technical_tokens"]).sort_values('count', ascending=False).head(10)
 soft_skills = count_tokens(skills["softskills_tokens"]).sort_values('count', ascending=False).head(5)
 education = count_tokens(skills["education_tokens"]).sort_values('count', ascending=False)
 
 fig1 = plot_horizontal(
-    technical_skills['tokens'], 
-    technical_skills['percentage'],
+    y=technical_skills['tokens'], 
+    x=technical_skills['percentage'],
     xlabel='Percentage',
     cmap='cool',
     xlim=(0,80),
     bar_label=True,
-    figsize=(6,3.5)
+    figsize=(6,3.5),
+    title="Technical Skills",
+    subtitle="The percentage of total jobs require these skills for a Data Analyst role."
     )
-st.pyplot(fig1, use_container_width=False)
 
+with st.container(border=True):
+    st.pyplot(fig1, use_container_width=False)
+    st.write(
+        """
+        These top 10 technical skills are more frequently sought by employers. In Data Analyst roles,
+        SQL is the most in-demand skill, appearing in 56.4% of job postings. Excel and Python follow, 
+        being required in 38% and 32.6% of roles, respectively, signifying their essential use in data
+        cleaning, and analysis. Visualization tools like Tableau and Power BI are also 
+        highly valued, appearing in over 20% of job postings, indicating a strong demand for
+        presenting insights effectively. Foundational concepts like statistics, along with 
+        niche skills like R, statistical analysis, and data mining, also play a significant
+        role.
+        """
+    )
 st.markdown("<br><br>", unsafe_allow_html=True)
-            
-col_fig2, col_fig3 = st.columns(2)
-with col_fig2:
-    fig2 = plot_horizontal(
-        soft_skills['tokens'], 
-        soft_skills['percentage'],
-        xlabel='Percentage',
-        cmap='plasma')
-    st.pyplot(fig2)
-with col_fig3:
-    fig3 = plot_horizontal(
-        education['tokens'], 
-        education['percentage'],
-        xlabel='Percentage')
-    st.pyplot(fig3)
+     
+fig2 = plot_horizontal(
+    y=education['tokens'], 
+    x=education['percentage'],
+    xlabel='Percentage',
+    figsize=(6,3),
+    bar_label=True,
+    xlim=(0, 80),
+    color='steelblue',
+    title='Education Requirement',
+    subtitle='The percentage of total jobs require univerty/college degree'
+    )
+with st.container(border=True):
+    st.pyplot(fig2, use_container_width=False)
+    st.write(
+        """
+         Majority of job postings (60.7%) require candidates to hold a Bachelor's degree, 
+         establishing it as the standard qualification for most roles. A Master's degree 
+         is the second most sought-after qualification, required by 19.8% of jobs, 
+         reflecting its importance in specialized or senior positions. 
+         Certifications (5.7%) and Diplomas (3.3%) are valued for niche roles or 
+         skill-based positions, while only 1.3% of job postings explicitly demand a PhD,
+         highlighting its relevance in highly specialized fields.
+        """)
--- strm/st_extends.py
+++ strm/st_extends.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-plt.style.use('ggplot')
+import plotly.express as px
 
 
 def count_tokens(series) -> pd.DataFrame:
@@ -18,25 +17,32 @@ def count_tokens(series) -> pd.DataFrame:
     return df
 
 
-def plot_horizontal(y, x, xlabel=None, ylabel=None, title=None, cmap=None, xlim=None, bar_label=False, figsize=None, ax=None, **kwargs):
+def plot_horizontal(y, x, xlabel=None, ylabel=None, color=None, title=None, subtitle=None, cmap=None, xlim=None, bar_label=False, figsize=None, ax=None, **kwargs):
     if ax is None:
         fig, ax = plt.subplots(figsize=figsize)
-    if cmap is None:
-        cmap = 'viridis'
-    color_mapping = plt.get_cmap(cmap)
-    norm = plt.Normalize(min(x), max(x))
-    colors = color_mapping(norm(x))
-    bars = ax.barh(y=y, width=x, color=colors, zorder=2, **kwargs)
+    if color is None:
+        if cmap is None:
+            cmap = 'viridis'
+        color_mapping = plt.get_cmap(cmap)
+        norm = plt.Normalize(min(x), max(x))
+        color = color_mapping(norm(x))
+    bars = ax.barh(y=y, width=x, color=color, zorder=2, **kwargs)
     if bar_label:
         ax.bar_label(bars, labels=[f'{height:.1f}%' for height in x], padding=3, fontsize=4.5)
     ax.invert_yaxis()
     ax.set_xlim(xlim)
     ax.set_xlabel(xlabel, fontsize=5)
-    ax.set_ylabel(ylabel)
-    ax.set_title(title)
+    ax.set_ylabel(ylabel, fontsize=5)
+    ax.set_title(subtitle, fontsize=6, fontweight='medium')
     ax.set_facecolor('#dbd7d2')
     ax.grid(linewidth=0.6, color='1', zorder=1)
     for spine in ax.spines.values():
         spine.set_color('white')
-    ax.tick_params(labelsize=5, bottom=False)
+    ax.tick_params(labelsize=5, bottom=False, left=False, pad=0)
+    fig.suptitle(title, fontsize=9, fontweight='bold')
+    return fig
+
+
+def plot_pie(data, values, names, title, **kwargs):
+    fig = px.pie(data, values=values, names=names, title=title, **kwargs)
     return fig
